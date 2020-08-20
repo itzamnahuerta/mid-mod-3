0e961c2e68285ee560856d95f06c711b79d38d2f
Itzamna Huerta <_@itzamna-huerta.myfiosgateway.com>
2020-08-20 13:49:12 -0400

    add dashboard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Dashboard from './components/Dashboard.jsx';
+
 function App() {
   return (
     <div className="App">
-      <h1> hello </h1>
+      <h1> RECYCLE TODAY </h1>
+      <Dashboard />
     </div>
   );
 }
