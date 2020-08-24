31908c2874865a5b3dc483acad7ecef601b1ba05
Itzamna Huerta <_@itzamna-huerta.local>
2020-08-24 11:18:32 -0500

    build

---
+++ build/precache-manifest.5885b48428e3ee7073f3d586276518cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6025816df412bdbea840b2f6d477442c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e90c4a67a3a504a5811",
+    "url": "/static/css/main.5a3dcebd.chunk.css"
+  },
+  {
+    "revision": "bcfb95f433925f6d00df",
+    "url": "/static/js/2.22a7f630.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22a7f630.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e90c4a67a3a504a5811",
+    "url": "/static/js/main.141a3628.chunk.js"
+  },
+  {
+    "revision": "93e36c1ef7d0da098c85",
+    "url": "/static/js/runtime-main.c95e08a1.js"
+  }
+]);
